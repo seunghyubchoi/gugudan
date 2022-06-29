0bd30568d7b5b5200e3b72c448471956f495495d
seunghyubchoi <seunghyubchoi@localhost>
2022-06-29 12:51:16 -0700

    처음부터 class 나누기 전까지 연

---
+++ src/gugudan/GugudanAgain.java
@@ -0,0 +1,26 @@
+package gugudan;
+
+public class GugudanAgain {
+	public static int[] calculate(int times) {
+		int result[] = new int[9];
+		for(int i = 0; i < result.length; i++) {
+			result[i] = times * (i + 1);
+		}
+		return result;
+	}
+
+public static void print(int[] result) {
+	for(int i = 0; i < result.length; i++) {
+		System.out.println(result[i]);
+		}
+}
+	
+	
+public static void main(String[] args) {
+	for(int i = 2; i < 10; i++) {
+	int[] times = calculate(i);
+	print(times);
+	System.out.println("");
+	}
+	}
+}
