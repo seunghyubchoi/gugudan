9f35239123dd34f2cb1fd4534aab24ee7adf8a4e
seunghyubchoi <seunghyubchoi@localhost>
2022-06-29 12:13:43 -0700

    class added

---
+++ src/gugudan/GugudanMain.java
@@ -0,0 +1,11 @@
+package gugudan;
+
+public class GugudanMain {
+	public static void main(String[] args) {
+		for(int i = 2; i < 10; i++) {
+			int[] times = method.calculate(i);
+			method.print(times);
+		}
+		
+		}
+}
--- src/gugudan/method.java
+++ src/gugudan/method.java
@@ -39,17 +39,9 @@ public static int[] calculate(int times) {
 public static void print(int[] result) {
 	for(int i = 0; i < result.length; i++) {
 		System.out.println(result[i]);
-	}
-}
-	
-
-public static void main(String[] args) {
-	for(int i = 2; i < 10; i++) {
-		int[] times = calculate(i);
-		print(times);
+		}
 	}
 	
-	}
 }
 
 	
