4b7910446f0cf1b4dbebfaa7f160af1af04640e7
seunghyubchoi <seunghyubchoi@seunghycbookpro.attlocal.net>
2022-06-28 18:06:30 -0700

    multiplication with array for

---
+++ src/gugudan/Gugudan.java
@@ -0,0 +1,20 @@
+package gugudan;
+
+public class Gugudan {
+	public static void main(String[] args) {
+		
+			int[] result = new int[9];
+				for(int j = 2; j < 10; j++) {
+					for(int i = 0; i < result.length; i++) {
+						result[i] = j * (i + 1);
+				}
+			
+					for(int i = 0; i < result.length; i++) {
+						System.out.println(result[i]);
+				
+					}
+				
+				}
+			}
+	}
+
